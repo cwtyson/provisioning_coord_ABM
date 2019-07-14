5d78bf84cb1baee645600c9b822ca88e26a14ca6
cwtyson <cwtyson@ucdavis.edu>
2019-07-14 10:32:11 +0100

    Update ct_working.R

--- ct_working.R
+++ ct_working.R
@@ -1,9 +1,34 @@
-shortTrip <- function()
-{
-
-}
-
-longTrip <- function()
-{
-	
-}
+## Go on a trip
+        trip <- function(parent, trip.type){
+          
+          ## We will assume two different foraging zones. 
+          ## One less productive, but closer and used during short trips. 
+          ## The other, farther, but more productive and used during long trips.
+          ## Amount gained each day should be a function of how:
+          ## 1) how productive the area is, 2) how far the individual has traveled, and 3) how successful the individual is while foraging
+          
+          ## If short trip, daily energy gain will be lower (less productive) and the average daily commuting cost will be higher (must fly there and back)
+          if(trip.type == "st"){
+            
+            ## Calculate flight cost given distance to ST foraging zone
+            
+            ## Calculate 
+            
+            
+            
+          }
+          
+          ## If long trip:
+          if(trip.type == "lt"){
+            
+            
+          }
+          
+          
+          ## At the end, return the parent with their body mass and 
+          
+          
+        }
+        
+        
+      }
